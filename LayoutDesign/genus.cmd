7c4023edca4adcad8b3a6e4d584f34d15e4dff49
CharanMV2913 <charankumarv2913@gmail.com>
2024-01-31 10:05:38 +0530

    BASIC GATES LAYOUT using GENERATE ALL FROM SOURCE

---
+++ LayoutDesign/genus.cmd
@@ -0,0 +1,8 @@
+# Cadence Genus(TM) Synthesis Solution, Version 20.11-s111_1, built Apr 26 2021 14:57:38
+
+# Date: Tue Dec 12 11:47:57 2023
+# Host: vvcevpn (x86_64 w/Linux 3.10.0-1160.el7.x86_64) (24cores*96cpus*2physical cpus*Intel(R) Xeon(R) Gold 5318N CPU @ 2.10GHz 36864KB)
+# OS:   CentOS Linux release 7.9.2009 (Core)
+
+write_template -simple outfile new.tcl
+write_template -simple -outfile new.tcl
